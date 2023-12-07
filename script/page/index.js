3a75b6ad96769b3fb902b2f07502886bf7538305
Samantha Halfon Faw <samantha.halfon@gmail.com>
2023-12-07 14:19:53 +0100

    Session mentorat

--- script/page/index.js
+++ script/page/index.js
@@ -2,15 +2,38 @@ async function renderRecipes(dataRecette){
     const recetteSection = document.getElementById ("section");
     document.querySelector(".listeRecettes").innerHTML="";
 
+    let ingredients = []
+
     dataRecette.forEach((recipe) => {
         const modelRecette = recetteFactory(recipe);
         const recetteCardDom = modelRecette.getFicheRecette();
         recetteSection.appendChild(recetteCardDom)
+        recipe.ingredients.forEach(ingr => {
+            if (!ingredients.includes(ingr.ingredient)) {
+                ingredients.push(ingr.ingredient)
+                li = document.createElement('LI')
+                check = document.createElement('INPUT')
+                check.type = 'checkbox'
+                check.value = ingr.ingredient
+                check.classList.add('checkbox_ingredient')
+                li.appendChild(check)
+                span = document.createElement('SPAN')
+                span.innerHTML = ingr.ingredient
+                li.appendChild(span)
+                document.getElementById('container_ingredients').appendChild(li)
+            }
+        })
+
     });
 }
 function getSearchParams(){
     let search_query = document.getElementById('field_search').value;
-    let search_ingredients = document.getElementById('ingredients_search').value; 
+    let search_ingredients = []
+    selected_ingredients = document.querySelectorAll('.checkbox_ingredient').forEach(check => {
+        if (check.checked) search_ingredients.push(check.value.toLowerCase())
+    })
+    console.log(search_ingredients)
+    // let search_ingredients = document.getElementById('ingredients_search').value; 
     let search_appareil = document.getElementById('appareils_search').value; 
     let search_ustensil = document.getElementById('ustensils_search').value; 
     return {
@@ -27,9 +50,10 @@ function searchRecipesClassic(recipes, searchParams) {
     for (let i = 0; i < recipes.length; i++) {
         const recipe = recipes[i];
         let ingredientMatch = true, appareilMatch = true, ustensilMatch = true;
-
+        console.log(recipe.ingredients)
         // Vérifier si les filtres sont définis et, le cas échéant, si la recette correspond au filtre
-        if (searchParams.search_filters.ingredients && !recipe.ingredients.some(ingredient => ingredient.ingredient.toLowerCase().includes(searchParams.search_filters.ingredients.toLowerCase()))) {
+        recipe_ingredient_list = recipe.ingredients.map(elt => elt.ingredient.toLowerCase())
+        if (searchParams.search_filters.ingredients && !searchParams.search_filters.ingredients.every(ingredient => recipe_ingredient_list.includes(ingredient))) {
             ingredientMatch = false;
         }
 
@@ -63,6 +87,28 @@ function initEvents() {
     document.getElementById('search_button').addEventListener('click', function(event) {        
         searchRecipes(recipes);
     });
+
+    document.querySelectorAll('.dropdown_btn').forEach(elt => elt.addEventListener('click', function(event) {
+        console.log(event.target)
+        console.log(event.target.classList)
+        let type = 'ingredients'
+        if (event.target.classList.contains('btn_appareils')) type = 'appareils'
+        if (event.target.classList.contains('btn_ustensils')) type = 'ustensils'        
+        console.log(type)
+    console.log(document.getElementById('filtre_' + type).style.display)
+        if (document.getElementById('filtre_' + type).style.display == 'none') {
+            // element is hidden, show it
+            document.getElementById('filtre_' + type).style.display = 'block';
+            document.getElementById(type + '_down').style.display = 'none';
+            document.getElementById(type + '_up').style.display = 'block';            
+        } else {
+            // element is visible, hide it
+            document.getElementById('filtre_' + type).style.display = 'none';
+            document.getElementById(type + '_down').style.display = 'block';
+            document.getElementById(type + '_up').style.display = 'none';            
+        }
+    }));
+
 }
 async function init(){
     renderRecipes(recipes);
